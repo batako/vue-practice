cb45967d910073b42692423f0a28d29058d5c31b
batako <guri725ren213@gmail.com>
2020-05-22 14:39:33 +0900

    ログインAPI実装
    https://qiita.com/Masahiro_T/items/6bc49a625b437a7c2f45

--- api/db/migrate/20200522043651_devise_token_auth_create_users.rb
+++ api/db/migrate/20200522043651_devise_token_auth_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    
+
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
@@ -36,6 +36,13 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
 
       ## Tokens
       t.text :tokens
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       t.timestamps
     end
