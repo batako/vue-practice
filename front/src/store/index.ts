301ec48ac8bb6a4768a2a2c1d61e23bf0120fc33
batako <guri725ren213@gmail.com>
2020-05-22 18:13:31 +0900

    vue-ls経由でローカルストレージに保存できるように

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Storage from 'vue-ls'
 import mutations from './mutations'
 
 Vue.use(Vuex)
+Vue.use(Storage, { storage: 'local' })
 
 export default new Vuex.Store({
   state: {
