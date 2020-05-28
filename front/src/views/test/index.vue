44b734e888abc44a8084c6969b37b85f163a610f
batako <guri725ren213@gmail.com>
2020-05-28 14:09:49 +0900

    vue-kinesis のサンプル追加

--- front/src/views/test/index.vue
+++ front/src/views/test/index.vue
@@ -3,6 +3,37 @@
     <h1>This is an test page</h1>
 
     <button @click="openModal">モーダル</button>
+
+    <kinesis-container>
+      <kinesis-element
+          tag="img"
+          :src="'https://www.kabanoki.net/wp-content/uploads/2020/01/agura_kutsurogu3_ojisan.png'"
+          :strength="-20"
+          type="rotate"
+          transformOrigin="50% 300%"
+          axis="x" />
+      <kinesis-element
+          tag="img"
+          :src="'https://www.kabanoki.net/wp-content/uploads/2020/01/agura_kutsurogu7_obasan.png'"
+          :strength="-10"
+          type="rotate"
+          transformOrigin="50% 300%"
+          axis="x" />
+      <kinesis-element
+          tag="img"
+          :src="'https://www.kabanoki.net/wp-content/uploads/2020/01/himan_pocchari07_ojisan.png'"
+          :strength="10"
+          type="rotate"
+          transformOrigin="50% 300%"
+          axis="x" />
+      <kinesis-element
+          tag="img"
+          :src="'https://www.kabanoki.net/wp-content/uploads/2020/01/himan_pocchari08_obasan.png'"
+          :strength="20"
+          type="rotate"
+          transformOrigin="50% 300%"
+          axis="x" />
+    </kinesis-container>
   </div>
 </template>
 
@@ -11,12 +42,19 @@ import {
     Component,
     Mixins,
   } from 'vue-property-decorator'
+import { KinesisContainer, KinesisElement} from 'vue-kinesis'
 
 import Title from '../../components/mixins/title'
 
 import SampleModal from '../../components/modal/sample.vue'
 
-@Component
+
+@Component({
+  components: {
+    KinesisContainer,
+    KinesisElement,
+  }
+})
 export default class TestIndex extends Mixins(Title) {
   title = 'テスト'
 
