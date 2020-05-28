6ade9bc2b44c4452ce602c6cdda7e3fae6223f99
batako <guri725ren213@gmail.com>
2020-05-28 16:39:02 +0900

    date-picker のサンプル追加

--- front/src/views/test/index.vue
+++ front/src/views/test/index.vue
@@ -3,8 +3,20 @@
     <h1>This is an test page</h1>
 
     <button @click="openModal">モーダル</button>
+
+    <div class="datetime">
+      <el-date-picker v-model="datetime"
+                      type="datetime"
+                      placeholder="日時を選択してください" />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.datetime {
+  margin-top: 10px;
+}
+</style>
 
 <script lang="ts">
 import {
@@ -19,6 +31,7 @@ import SampleModal from '../../components/modal/sample.vue'
 @Component
 export default class TestIndex extends Mixins(Title) {
   title = 'テスト'
+  datetime = null
 
   openModal() {
     this.$modal.show(
