a8acd64a200b46e0533b98a683023e5adaccf4f7
batako <guri725ren213@gmail.com>
2020-06-01 08:33:01 +0900

    dumy

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,26 +2,32 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import { ShareModule } from '../store/modules/share'
 
+import Layout from '@/layout'
+
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
+    component: Layout,
     name: 'home',
     component: () => import('@/views/home/index.vue')
   },
   {
     path: '/about',
+    component: Layout,
     name: 'about',
     component: () => import('@/views/about/index.vue')
   },
   {
     path: '/login',
+    component: Layout,
     name: 'login',
     component: () => import('@/views/login/index.vue')
   },
   {
     path: '/sample',
+    component: Layout,
     name: 'sample',
     component: () => import('@/views/sample/index.vue')
   },
