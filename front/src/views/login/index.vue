874b9a93dccd2f63b1078ae743706d98d91adc40
batako <guri725ren213@gmail.com>
2020-06-07 02:38:39 +0900

    ログインで composition-api を用いるように

--- front/src/views/login/index.vue
+++ front/src/views/login/index.vue
@@ -29,52 +29,53 @@
 
 <script lang="ts">
 /* composition-api */
-// import {
-//     reactive,
-//     onMounted,
-//     toRefs,
-//   } from '@vue/composition-api'
-// import router from '@/router'
+import {
+    defineComponent,
+    onMounted,
+    reactive,
+    toRefs,
+  } from '@vue/composition-api'
+import router from '@/router'
 
-// import AuthService from '../../services/auth'
-// // import Title from '../../components/mixins/title'
-// import { ShareModule } from '../../store/modules/share'
+import AuthService from '../../services/auth'
+// import Title from '../../components/mixins/title'
+import { ShareModule } from '../../store/modules/share'
 
-// export default {
-//   setup() {
-//     // const title = 'ログイン'
-//     const state = reactive({
-//       email:    'example@example.com',
-//       password: 'password',
-//     })
-
-
-//     function _init() {
-//       AuthService.logout()
-//     }
+export default defineComponent({
+  setup() {
+    // const title = 'ログイン'
+    const state = reactive({
+      email:    'example@example.com',
+      password: 'password',
+    })
 
 
-//     function login() {
-//       AuthService
-//         .login(state.email, state.password)
-//         .then((response: any) => {
-//           router.push('/')
-//           ShareModule.clearToastrs()
-//         })
-//     }
+    function _init() {
+      AuthService.logout()
+    }
 
 
-//     onMounted(() => {
-//       _init()
-//     })
+    function login() {
+      AuthService
+        .login(state.email, state.password)
+        .then((response: any) => {
+          router.push('/')
+          ShareModule.clearToastrs()
+        })
+    }
 
 
-//     return {
-//       login,
-//       ...toRefs(state),
-//     }
-//   }
-// }
+    onMounted(() => {
+      _init()
+    })
+
+
+    return {
+      login,
+      ...toRefs(state),
+    }
+  }
+})
 
 
 /* vue-property-decorator */
@@ -122,49 +123,49 @@
 
 
 /* options api */
-import Vue from 'vue'
+// import Vue from 'vue'
 
-// mixin ファイルは options api 記法でも vue-property-decorator 記法でも動く
-// import Title from '../../components/mixins/title'
-import Title from '../../components/mixins/title.legacy'
+// // mixin ファイルは options api 記法でも vue-property-decorator 記法でも動く
+// // import Title from '../../components/mixins/title'
+// import Title from '../../components/mixins/title.legacy'
 
-import AuthService from '../../services/auth'
-import { ShareModule } from '../../store/modules/share'
+// import AuthService from '../../services/auth'
+// import { ShareModule } from '../../store/modules/share'
 
-export default Vue.extend({
-  mixins: [
-    Title,
-  ],
-  data() {
-    return {
-      title:    'ログイン',
-      email:    'example@example.com',
-      password: 'password',
-    }
-  },
-  mounted() {
-    this.$_init()
-  },
-  methods: {
-    $_init() {
-      AuthService.logout()
-    },
-    login() {
-      AuthService
-        .login(this.email, this.password)
-        .then((response: any) => {
-          this.$router.push('/')
-          ShareModule.clearToastrs()
-        })
-    },
-  },
-  // watch: {
-  //   email(newVlue = '') {
-  //     this.email = newVlue.replace(
-  //       /[Ａ-Ｚａ-ｚ０-９]/g,
-  //       function(s: string) { return String.fromCharCode(s.charCodeAt(0) - 65248).replace(/^[^0-9a-zA-Z]*$/g, '') }
-  //     )
-  //   },
-  // },
-})
+// export default Vue.extend({
+//   mixins: [
+//     Title,
+//   ],
+//   data() {
+//     return {
+//       title:    'ログイン',
+//       email:    'example@example.com',
+//       password: 'password',
+//     }
+//   },
+//   mounted() {
+//     this.$_init()
+//   },
+//   methods: {
+//     $_init() {
+//       AuthService.logout()
+//     },
+//     login() {
+//       AuthService
+//         .login(this.email, this.password)
+//         .then((response: any) => {
+//           this.$router.push('/')
+//           ShareModule.clearToastrs()
+//         })
+//     },
+//   },
+//   // watch: {
+//   //   email(newVlue = '') {
+//   //     this.email = newVlue.replace(
+//   //       /[Ａ-Ｚａ-ｚ０-９]/g,
+//   //       function(s: string) { return String.fromCharCode(s.charCodeAt(0) - 65248).replace(/^[^0-9a-zA-Z]*$/g, '') }
+//   //     )
+//   //   },
+//   // },
+// })
 </script>
