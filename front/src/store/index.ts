5dc694fb3219b3e9c42fcaa9de2c931895ccbd13
batako <guri725ren213@gmail.com>
2020-06-07 01:34:15 +0900

    vue-ls の読み込みを外部ファイルに

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Storage from 'vue-ls'
 
 import { ShareState } from './modules/share'
 
 Vue.use(Vuex)
-Vue.use(Storage, { storage: 'local' })
 
 export interface RootState {
   share: ShareState;
