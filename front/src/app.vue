3470c9ce37599aab34f30fe05b039859614f55f4
batako <guri725ren213@gmail.com>
2020-06-12 01:24:59 +0900

    navbar を非表示に

--- front/src/app.vue
+++ front/src/app.vue
@@ -3,7 +3,7 @@ import Navbar from '@/components/shared/navbar';
   <div id="app">
     <loading />
     <toastrs />
-    <navbar />
+    <!-- <navbar /> -->
     <router-view/>
   </div>
 </template>
