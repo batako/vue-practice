7a7f73f1617146ab70e25f8678f0fca8e969b561
batako <guri725ren213@gmail.com>
2020-06-12 02:28:35 +0900

    トップページのイメージ作成

--- front/src/views/home/index.vue
+++ front/src/views/home/index.vue
@@ -1,7 +1,44 @@
 <template>
   <app-layout>
     <template>
-      @contents
+      <v-row>
+        <v-col cols="12" sm="6" offset-sm="3">
+          <v-card
+            v-for="n in 10"
+            :key="n"
+            cols="12"
+            class="article"
+          >
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                <v-icon class="user-icon">mdi-account</v-icon>
+                <span class="user-name">user {{n}}</span>
+              </v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </template>
 
     <template #bottom-content>
@@ -25,6 +62,24 @@
     </template>
   </app-layout>
 </template>
+
+<style lang="scss" scoped>
+.article {
+  margin-bottom: 60px;
+
+  .user-icon {
+    background: white;
+    border-radius: 50%;
+  }
+  .user-name {
+    margin-left: 16px;
+    font-size: 14px;
+    font-weight: 400;
+    font-stretch: 100%;
+    line-height: 14px;
+  }
+}
+</style>
 
 <script>
 import {
