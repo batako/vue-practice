442d18d1ee58bf92e78b07457ff3fa56f1f1c721
batako <guri725ren213@gmail.com>
2020-06-11 18:33:51 +0900

    ログインを vuetify で置き換え

--- front/src/views/login/index.vue
+++ front/src/views/login/index.vue
@@ -1,26 +1,61 @@
 <template>
-  <div class="login">
-    <div class = "container">
-      <div class="wrapper">
-        <hr class="colorgraph"><br>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="E-Mail"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="email"
+                    v-model="email"
+                  ></v-text-field>
 
-        <form @submit.prevent="login">
-          <input type="email"
-                 class="form-control"
-                 placeholder="メールアドレス"
-                 v-model="email">
-
-          <input type="password"
-                 class="form-control"
-                 placeholder="パスワード"
-                 v-model="password">
-
-          <input type="submit"
-                 value="ログイン">
-        </form>
-      </div>
-    </div>
-  </div>
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    v-model="password"
+                    @keyup.enter="login()"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  @click="login()"
+                >Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss" scoped>
