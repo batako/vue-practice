d235f4bfc17fd4c250a5e432749bc8f5cc7bb2a1
batako <guri725ren213@gmail.com>
2020-06-12 14:06:32 +0900

    api 全て json 形式でレスポンスを返すように

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 
-  namespace :api do
-    resources :articles, only: [:create]
+  namespace :api, { format: 'json' } do
+    resources :articles, only: [:index, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
