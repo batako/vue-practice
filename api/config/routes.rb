6f1d494b52781465ae19c2320746d44688f0a563
batako <guri725ren213@gmail.com>
2020-06-12 11:54:34 +0900

    サーバーの画像投稿の口のみ追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   scope :api do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
+
+  namespace :api do
+    resources :articles, only: [:create]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
