dc753924ab1c1b97021ab9f6ac513b5bcbb9089a
batako <guri725ren213@gmail.com>
2020-06-13 18:36:00 +0900

    記事の画像の縦幅に制限をかけないように

--- front/src/views/home/index.vue
+++ front/src/views/home/index.vue
@@ -14,7 +14,6 @@
               :src="airticle.photo"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
             >
               <v-card-title>
                 <v-img
