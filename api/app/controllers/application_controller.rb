39374a7f5be1c5eeba47e2eb239c2a9c792a3a7e
batako <guri725ren213@gmail.com>
2020-06-14 13:44:21 +0900

    リロード対策＆400対策
    画面のルーティングを vue 側で制御するように

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include CurrentRequest
+
+  def index
+    render file: Rails.root.join('public/index.html'),
+           status: 200,
+           layout: false,
+           content_type: 'text/html'
+  end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       sessions:  'api/users/sessions'
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
     end
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*vue_routes', to: 'application#index'
 end
